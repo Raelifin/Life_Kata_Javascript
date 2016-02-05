4b136ba79ae363581913fda9cfdb432a65fe4858
Raelifin <raelifin@gmail.com>
2016-02-05 13:17:39 -0500

    Added cells with correct content to box

--- automatonRenderer.js
+++ automatonRenderer.js
@@ -1,12 +1,14 @@
 function AutomatonController() {
-	this.createAutomatonBox = function() {
+	this.createAutomatonBox = function(board) {
 		var resultHTML = '<div id="automatonBox">';
 		resultHTML += '<h1>Conway\'s Life</h1>';
 		resultHTML += '<div id="automatonCells">';
-		for (var y=0; y < 6; y++) {
+		for (var y=0; y < board.length; y++) {
 			resultHTML += '<div class="row">';
-			for (var x=0; x < 8; x++) {
-				resultHTML += '<div class="cell"></div>';
+			for (var x=0; x < board[y].length; x++) {
+				resultHTML += '<div class="cell">';
+				resultHTML += board[y][x];
+				resultHTML += '</div>';
 			}
 			resultHTML += '</div>';
 		}
--- test_automatonRenderer.js
+++ test_automatonRenderer.js
@@ -12,11 +12,28 @@ var sample8by6BoardAtTime0 = [
 var r = new AutomatonController();
 
 QUnit.test("Renderer can create automatonBox div with internal elements", function(assert) {
-	$('body').append(r.createAutomatonBox());
+	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
 	assert.ok($('#automatonBox').length == 1);
 	assert.ok($('#automatonBox h1').length == 1);
 	assert.ok($('#automatonBox #automatonCells').length == 1);
 	assert.ok($('#automatonBox #resetAutomatonButton').length == 1);
 	assert.ok($('#automatonBox .row').length == 6);
 	assert.ok($('#automatonBox .cell').length == 48);
+	$('#automatonBox').remove();
+});
+
+QUnit.test("Box header is labeled \"Conway's Life\"", function(assert) {
+	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
+	assert.ok($('#automatonBox h1').html() == 'Conway\'s Life');
+	$('#automatonBox').remove();
+});
+
+QUnit.test("AutomatonBox has cells with correct content", function(assert) {
+	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
+	$('#automatonBox .row').each(function(y) {
+		$(this).children('.cell').each(function(x) {
+			assert.ok($(this).html() == sample8by6BoardAtTime0[y][x]);
+		});
+	});
+	$('#automatonBox').remove();
 });
