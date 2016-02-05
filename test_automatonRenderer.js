a8aee6cefe223b491a204b3cc1ed0ad5ffaa0719
Raelifin <raelifin@gmail.com>
2016-02-05 12:35:38 -0500

    Linked jQuery to test environment

--- test_automatonRenderer.js
+++ test_automatonRenderer.js
@@ -1,6 +1,8 @@
 //Not bothering to test the html for this exercise.
 
-
+$( document ).ready(function() {
+	$('html').append('<div id="lifeBox"><h1>Conway\'s Life</h1><canvas id="lifeCanvas"></canvas><button id="resetLifeButton">Reset</button></div>');
+});
 
 QUnit.test("Placeholder test", function(assert) {
 	assert.ok(1==1);
