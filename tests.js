16e48665f5962cbd71ca39b1fbf84464f2a9ddb4
Raelifin <raelifin@gmail.com>
2016-02-05 08:59:51 -0500

    Extended neighborhood test coverage

--- tests.js
+++ tests.js
@@ -17,10 +17,19 @@ const sample8by6BoardAtTime1 = [
 	[d,d,d,a,a,d,d,d],
 	[d,d,d,a,a,d,d,d]];
 
-QUnit.test("2 living neighbors and 1 dead neighbor at corner of sample board", function( assert ) {
+QUnit.test("Sample board has expected neighborhoods", function( assert ) {
 	var neighbors = lifeNeighbors(sample8by6BoardAtTime0,0,0);
-	console.log(neighbors);
 	assert.ok(neighbors.length == 3);
 	assert.ok(neighbors.filter(x=>x==a).length == 2);
 	assert.ok(neighbors.filter(x=>x==d).length == 1);
+	
+	neighbors = lifeNeighbors(sample8by6BoardAtTime0,4,2);
+	assert.ok(neighbors.length == 8);
+	assert.ok(neighbors.filter(x=>x==a).length == 0);
+	assert.ok(neighbors.filter(x=>x==d).length == 8);
+	
+	neighbors = lifeNeighbors(sample8by6BoardAtTime0,2,4);
+	assert.ok(neighbors.length == 8);
+	assert.ok(neighbors.filter(x=>x==a).length == 2);
+	assert.ok(neighbors.filter(x=>x==d).length == 6);
 });
