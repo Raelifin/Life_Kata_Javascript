a2a38c8c4fe74c656f76ff1112c0ca38e31eaf4e
Raelifin <raelifin@gmail.com>
2016-02-05 12:08:28 -0500

    Moved life functions into generalized automata class

--- life.js
+++ life.js
@@ -1,22 +1,25 @@
-var lifeRules = {
+function Automata2D(rules) {
+	this.rules = rules;
+	
+	var neighborDeltas = [
+		[-1,-1],[+0,-1],[+1,-1],
+		[-1,+0],        [+1,+0],
+		[-1,+1],[+0,+1],[+1,+1]];
+	this.neighbors = function(board,x,y) {
+		var validDeltas = neighborDeltas.filter(d => board[y+d[1]] != undefined && board[y+d[1]][x+d[0]] != undefined);
+		return validDeltas.map(d => board[y+d[1]][x+d[0]]);
+	}
+	
+	this.nextCell = function(oldState, neighbors) {
+		return (oldState in this.rules) ? this.rules[oldState](neighbors) : oldState;
+	}
+	
+	this.nextBoard = function(board) {
+		return board.map( (row,y,a1) => row.map( (cell,x,a2) => this.nextCell(cell,this.neighbors(board,x,y)) ) );
+	}
+}
+
+var conwaysLife = new Automata2D({
 	ALIVE: function(neighbors) { return ([2,3].indexOf(neighbors.filter(x=> x == 'ALIVE').length) >= 0 ? 'ALIVE' : 'DEAD'); },
 	DEAD: function(neighbors) { return (neighbors.filter(x=> x == 'ALIVE').length == 3 ? 'ALIVE' : 'DEAD'); },
-};
-
-var lifeNeighborDeltas = [
-	[-1,-1],[+0,-1],[+1,-1],
-	[-1,+0],        [+1,+0],
-	[-1,+1],[+0,+1],[+1,+1]];
-
-function lifeNeighbors(board, x, y) {
-	var validDeltas = lifeNeighborDeltas.filter(d => board[y+d[1]] != undefined && board[y+d[1]][x+d[0]] != undefined);
-	return validDeltas.map(d => board[y+d[1]][x+d[0]]);
-}
-
-function lifeNextCell(oldState, neighbors) {
-	return (oldState in lifeRules) ? lifeRules[oldState](neighbors) : oldState;
-}
-
-function lifeNextBoard(board) {
-	return board.map( (thisRow,y,a1) => thisRow.map( (thisCell,x,a2) => lifeNextCell(thisCell,lifeNeighbors(board,x,y)) ) );
-}
+});
--- tests.js
+++ tests.js
@@ -18,35 +18,35 @@ const sample8by6BoardAtTime1 = [
 	[d,d,d,a,a,d,d,d]];
 
 QUnit.test("Sample board has expected neighborhoods", function(assert) {
-	var neighbors = lifeNeighbors(sample8by6BoardAtTime0,0,0);
+	var neighbors = conwaysLife.neighbors(sample8by6BoardAtTime0,0,0);
 	assert.ok(neighbors.length == 3);
 	assert.ok(neighbors.filter(x=>x==a).length == 2);
 	assert.ok(neighbors.filter(x=>x==d).length == 1);
 	
-	neighbors = lifeNeighbors(sample8by6BoardAtTime0,4,2);
+	neighbors = conwaysLife.neighbors(sample8by6BoardAtTime0,4,2);
 	assert.ok(neighbors.length == 8);
 	assert.ok(neighbors.filter(x=>x==a).length == 0);
 	assert.ok(neighbors.filter(x=>x==d).length == 8);
 	
-	neighbors = lifeNeighbors(sample8by6BoardAtTime0,2,4);
+	neighbors = conwaysLife.neighbors(sample8by6BoardAtTime0,2,4);
 	assert.ok(neighbors.length == 8);
 	assert.ok(neighbors.filter(x=>x==a).length == 2);
 	assert.ok(neighbors.filter(x=>x==d).length == 6);
 });
 
 QUnit.test("Life function gives correct mapping", function(assert) {
-	assert.ok(lifeNextCell(a, [a,a,d,d,a,d,d,d]) == a);
-	assert.ok(lifeNextCell(d, [a,a,d,d,a,d,d,d]) == a);
-	assert.ok(lifeNextCell(a, [d,d,d,d,a,d,d,a]) == a);
-	assert.ok(lifeNextCell(d, [d,d,d,d,a,d,d,a]) == d);
-	assert.ok(lifeNextCell(a, [d,d,d,d,a,d,d,d]) == d);
-	assert.ok(lifeNextCell(d, [d,d,d,d,a,d,d,d]) == d);
-	assert.ok(lifeNextCell(a, [a,a,a,a,a,d,d,d]) == d);
-	assert.ok(lifeNextCell(d, [a,a,a,a,a,d,d,d]) == d);
+	assert.ok(conwaysLife.nextCell(a, [a,a,d,d,a,d,d,d]) == a);
+	assert.ok(conwaysLife.nextCell(d, [a,a,d,d,a,d,d,d]) == a);
+	assert.ok(conwaysLife.nextCell(a, [d,d,d,d,a,d,d,a]) == a);
+	assert.ok(conwaysLife.nextCell(d, [d,d,d,d,a,d,d,a]) == d);
+	assert.ok(conwaysLife.nextCell(a, [d,d,d,d,a,d,d,d]) == d);
+	assert.ok(conwaysLife.nextCell(d, [d,d,d,d,a,d,d,d]) == d);
+	assert.ok(conwaysLife.nextCell(a, [a,a,a,a,a,d,d,d]) == d);
+	assert.ok(conwaysLife.nextCell(d, [a,a,a,a,a,d,d,d]) == d);
 });
 
 QUnit.test("Next board function gives correct mapping on sample 8x6 input", function(assert) {
-	var nextBoard = lifeNextBoard(sample8by6BoardAtTime0);
+	var nextBoard = conwaysLife.nextBoard(sample8by6BoardAtTime0);
 	for (var y=0; y < 6; y++) {
 		for (var x=0; x < 8; x++) {
 			assert.ok(nextBoard[y][x] == sample8by6BoardAtTime1[y][x]);
