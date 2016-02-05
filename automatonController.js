14e420396b84af7cda8b4e7c678793a838b1a939
Raelifin <raelifin@gmail.com>
2016-02-05 13:49:00 -0500

    Added step button functionality

--- automatonController.js
+++ automatonController.js
@@ -1,4 +1,6 @@
-function AutomatonController() {
+function AutomatonController(automaton) {
+	this.automaton = automaton;
+	
 	this.createAutomatonBox = function(board) {
 		var resultHTML = '<div id="automatonBox">';
 		resultHTML += '<h1>Conway\'s Life</h1>';
@@ -15,5 +17,22 @@ function AutomatonController() {
 		resultHTML += '<button id="stepAutomatonButton">Step</button>';
 		resultHTML += '</div>';
 		return resultHTML;
+	}
+	
+	this.step = function() {
+		var board = $('#automatonBox .row').map(function(y) {
+			return [$(this).children('.cell').map(function(x) { return $(this).html(); }).get()];
+		}).get();
+		$('#automatonBox #automatonCells').empty();
+		board = this.automaton.nextBoard(board);
+		var resultHTML = '';
+		for (var y=0; y < board.length; y++) {
+			resultHTML += '<div class="row">';
+			for (var x=0; x < board[y].length; x++) {
+				resultHTML += '<div class="cell automaton_'+board[y][x]+'">'+board[y][x]+'</div>';
+			}
+			resultHTML += '</div>';
+		}
+		$('#automatonBox #automatonCells').append(resultHTML);
 	}
 }
--- test_automatonController.js
+++ test_automatonController.js
@@ -9,10 +9,12 @@ var sample8by6BoardAtTime0 = [
 	[d,d,d,a,a,d,d,d],
 	[d,d,d,a,a,d,d,d]];
 
-var r = new AutomatonController();
+var r = new AutomatonController(conwaysLife);
 
 QUnit.test("Renderer can create automatonBox div with internal elements", function(assert) {
 	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
+	$('#automatonBox #stepAutomatonButton').click(function() { r.step(); });
+	
 	assert.equal($('#automatonBox').length, 1);
 	assert.equal($('#automatonBox h1').length, 1);
 	assert.equal($('#automatonBox #automatonCells').length, 1);
@@ -20,22 +22,43 @@ QUnit.test("Renderer can create automatonBox div with internal elements", functi
 	assert.equal($('#automatonBox #stepAutomatonButton').length, 1);
 	assert.equal($('#automatonBox .row').length, 6);
 	assert.equal($('#automatonBox .cell').length, 48);
+	
 	$('#automatonBox').remove();
 });
 
 QUnit.test("Box header is labeled \"Conway's Life\"", function(assert) {
 	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
+	$('#automatonBox #stepAutomatonButton').click(function() { r.step(); });
+	
 	assert.equal($('#automatonBox h1').html(), 'Conway\'s Life');
+	
 	$('#automatonBox').remove();
 });
 
 QUnit.test("AutomatonBox has cells with correct content and class", function(assert) {
 	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
+	$('#automatonBox #stepAutomatonButton').click(function() { r.step(); });
+	
 	$('#automatonBox .row').each(function(y) {
 		$(this).children('.cell').each(function(x) {
 			assert.equal($(this).html(), sample8by6BoardAtTime0[y][x]);
 			assert.ok($(this).hasClass('automaton_'+sample8by6BoardAtTime0[y][x]));
 		});
 	});
+	
+	$('#automatonBox').remove();
+});
+
+QUnit.test("StepAutomaton button works", function(assert) {
+	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
+	$('#automatonBox #stepAutomatonButton').click(function() { r.step(); });
+	
+	$('#automatonBox #stepAutomatonButton').click();
+	$('#automatonBox .row').each(function(y) {
+		$(this).children('.cell').each(function(x) {
+			assert.equal($(this).html(), sample8by6BoardAtTime1[y][x]);
+		});
+	});
+	
 	$('#automatonBox').remove();
 });
