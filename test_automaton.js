15d97cd4f480c934bc13bccc489ba48c0a148bac
Raelifin <raelifin@gmail.com>
2016-02-05 13:23:26 -0500

    Changed tests to .equal instead of .ok

--- test_automaton.js
+++ test_automaton.js
@@ -19,37 +19,37 @@ var sample8by6BoardAtTime1 = [
 
 QUnit.test("Sample board has expected neighborhoods", function(assert) {
 	var neighbors = conwaysLife.neighbors(sample8by6BoardAtTime0,0,0);
-	assert.ok(neighbors.length == 3);
-	assert.ok(neighbors.filter(x=>x==a).length == 2);
-	assert.ok(neighbors.filter(x=>x==d).length == 1);
+	assert.equal(neighbors.length, 3);
+	assert.equal(neighbors.filter(x=>x==a).length, 2);
+	assert.equal(neighbors.filter(x=>x==d).length, 1);
 	
 	neighbors = conwaysLife.neighbors(sample8by6BoardAtTime0,4,2);
-	assert.ok(neighbors.length == 8);
-	assert.ok(neighbors.filter(x=>x==a).length == 0);
-	assert.ok(neighbors.filter(x=>x==d).length == 8);
+	assert.equal(neighbors.length, 8);
+	assert.equal(neighbors.filter(x=>x==a).length, 0);
+	assert.equal(neighbors.filter(x=>x==d).length, 8);
 	
 	neighbors = conwaysLife.neighbors(sample8by6BoardAtTime0,2,4);
-	assert.ok(neighbors.length == 8);
-	assert.ok(neighbors.filter(x=>x==a).length == 2);
-	assert.ok(neighbors.filter(x=>x==d).length == 6);
+	assert.equal(neighbors.length, 8);
+	assert.equal(neighbors.filter(x=>x==a).length, 2);
+	assert.equal(neighbors.filter(x=>x==d).length, 6);
 });
 
 QUnit.test("Life function gives correct mapping", function(assert) {
-	assert.ok(conwaysLife.nextCell(a, [a,a,d,d,a,d,d,d]) == a);
-	assert.ok(conwaysLife.nextCell(d, [a,a,d,d,a,d,d,d]) == a);
-	assert.ok(conwaysLife.nextCell(a, [d,d,d,d,a,d,d,a]) == a);
-	assert.ok(conwaysLife.nextCell(d, [d,d,d,d,a,d,d,a]) == d);
-	assert.ok(conwaysLife.nextCell(a, [d,d,d,d,a,d,d,d]) == d);
-	assert.ok(conwaysLife.nextCell(d, [d,d,d,d,a,d,d,d]) == d);
-	assert.ok(conwaysLife.nextCell(a, [a,a,a,a,a,d,d,d]) == d);
-	assert.ok(conwaysLife.nextCell(d, [a,a,a,a,a,d,d,d]) == d);
+	assert.equal(conwaysLife.nextCell(a, [a,a,d,d,a,d,d,d]), a);
+	assert.equal(conwaysLife.nextCell(d, [a,a,d,d,a,d,d,d]), a);
+	assert.equal(conwaysLife.nextCell(a, [d,d,d,d,a,d,d,a]), a);
+	assert.equal(conwaysLife.nextCell(d, [d,d,d,d,a,d,d,a]), d);
+	assert.equal(conwaysLife.nextCell(a, [d,d,d,d,a,d,d,d]), d);
+	assert.equal(conwaysLife.nextCell(d, [d,d,d,d,a,d,d,d]), d);
+	assert.equal(conwaysLife.nextCell(a, [a,a,a,a,a,d,d,d]), d);
+	assert.equal(conwaysLife.nextCell(d, [a,a,a,a,a,d,d,d]), d);
 });
 
 QUnit.test("Next board function gives correct mapping on sample 8x6 input", function(assert) {
 	var nextBoard = conwaysLife.nextBoard(sample8by6BoardAtTime0);
 	for (var y=0; y < 6; y++) {
 		for (var x=0; x < 8; x++) {
-			assert.ok(nextBoard[y][x] == sample8by6BoardAtTime1[y][x]);
+			assert.equal(nextBoard[y][x], sample8by6BoardAtTime1[y][x]);
 		}
 	}
 });
--- test_automatonRenderer.js
+++ test_automatonRenderer.js
@@ -13,18 +13,18 @@ var r = new AutomatonController();
 
 QUnit.test("Renderer can create automatonBox div with internal elements", function(assert) {
 	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
-	assert.ok($('#automatonBox').length == 1);
-	assert.ok($('#automatonBox h1').length == 1);
-	assert.ok($('#automatonBox #automatonCells').length == 1);
-	assert.ok($('#automatonBox #resetAutomatonButton').length == 1);
-	assert.ok($('#automatonBox .row').length == 6);
-	assert.ok($('#automatonBox .cell').length == 48);
+	assert.equal($('#automatonBox').length, 1);
+	assert.equal($('#automatonBox h1').length, 1);
+	assert.equal($('#automatonBox #automatonCells').length, 1);
+	assert.equal($('#automatonBox #resetAutomatonButton').length, 1);
+	assert.equal($('#automatonBox .row').length, 6);
+	assert.equal($('#automatonBox .cell').length, 48);
 	$('#automatonBox').remove();
 });
 
 QUnit.test("Box header is labeled \"Conway's Life\"", function(assert) {
 	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
-	assert.ok($('#automatonBox h1').html() == 'Conway\'s Life');
+	assert.equal($('#automatonBox h1').html(), 'Conway\'s Life');
 	$('#automatonBox').remove();
 });
 
@@ -32,7 +32,7 @@ QUnit.test("AutomatonBox has cells with correct content and class", function(ass
 	$('body').append(r.createAutomatonBox(sample8by6BoardAtTime0));
 	$('#automatonBox .row').each(function(y) {
 		$(this).children('.cell').each(function(x) {
-			assert.ok($(this).html() == sample8by6BoardAtTime0[y][x]);
+			assert.equal($(this).html(), sample8by6BoardAtTime0[y][x]);
 			assert.ok($(this).hasClass('automaton_'+sample8by6BoardAtTime0[y][x]));
 		});
 	});
